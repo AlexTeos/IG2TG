a6432dfb024c1841019c706476e87c749a226904
AlexTeos <alex.teos@yandex.ru>
2023-11-04 21:25:42 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module ig2tg
+
+go 1.20
+
+require (
+	github.com/Davincible/goinsta/v3 v3.2.6 // indirect
+	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.22 // indirect
+	github.com/chromedp/cdproto v0.0.0-20230901104747-bfe71bcbd1c0 // indirect
+	github.com/chromedp/chromedp v0.9.2 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.3.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	gopkg.in/telebot.v3 v3.1.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Davincible/goinsta/v3"
+	tele "gopkg.in/telebot.v3"
+)
+
+type instaType struct {
+	last_time int64
+	cache     string
+	insta     *goinsta.Instagram
+}
+
+func newInstaTypeCache(cache_file string) instaType {
+	insta := instaType{last_time: 0, cache: cache_file}
+	insta.insta, _ = goinsta.Import(insta.cache, true)
+	insta.insta.OpenApp()
+	defer insta.insta.Export(insta.cache)
+	return insta
+}
+
+func newInstaTypeLogin(login string, password string) instaType {
+	insta := instaType{last_time: 0, cache: "../insta.cache"}
+	insta.insta = goinsta.New(login, password)
+	if err := insta.insta.Login(); err != nil {
+		panic(err)
+	}
+	defer insta.insta.Export(insta.cache)
+	return insta
+}
+
+func posts(insta *instaType) <-chan *goinsta.Item {
+	сhannel := make(chan *goinsta.Item)
+
+	go func() {
+		for {
+			items := insta.insta.Timeline.Items
+			sort.Slice(items, func(i, j int) bool {
+				return items[i].TakenAt < items[j].TakenAt
+			})
+			for i := 0; i < len(items); i++ {
+				if insta.last_time < items[i].TakenAt {
+					insta.last_time = items[i].TakenAt
+					сhannel <- items[i]
+				}
+			}
+
+			time.Sleep(5 * time.Second)
+			if err := insta.insta.Timeline.Refresh(); err != nil {
+				fmt.Printf("Refresh error: %s\n", err.Error())
+			}
+		}
+		//close(сhannel)
+	}()
+
+	return сhannel
+}
+
+func isCommercial(item *goinsta.Item) bool {
+	return item.CommercialityStatus != "not_commercial" || item.ProductType == "ad"
+}
+
+func main() {
+	cache := "../insta.cache"
+	var insta instaType
+	if _, err := os.Stat(cache); err == nil {
+		insta = newInstaTypeCache(cache)
+	} else {
+		insta = newInstaTypeLogin("login", "pass")
+	}
+
+	token := "bot_token"
+	pref := tele.Settings{
+		Token:  token,
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		return
+	}
+
+	var user tele.User
+	user.ID = 0
+
+	for item := range posts(&insta) {
+		if isCommercial(item) {
+			continue
+		}
+		item.DownloadTo("../media/0")
+
+		var album tele.Album
+		files, _ := filepath.Glob("../media/0*")
+		for i, file := range files {
+			if strings.Contains(file, "jpg") || strings.Contains(file, "heic") {
+				album = append(album, &tele.Photo{File: tele.FromDisk(file)})
+			}
+			if strings.Contains(file, "mp4") {
+				album = append(album, &tele.Video{File: tele.FromDisk(file)})
+			}
+
+			if len(album) == 10 || (len(album) != 0 && i == len(files)-1) {
+				_, err = b.SendAlbum(&user, album)
+				if err != nil {
+					fmt.Println(err)
+				}
+				album = nil
+			}
+		}
+
+		if len(item.Caption.Text) != 0 {
+			text := "TakenAt: " + time.Unix(item.TakenAt, 0).String()
+			text += "\nCommercialityStatus: " + item.CommercialityStatus
+			text += "\nProductType: " + item.ProductType
+			text += "\nFollowing: " + strconv.FormatBool(item.User.Friendship.Following)
+			text += "\nCaption: " + item.Caption.Text
+			_, err = b.Send(&user, text)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+		files, _ = filepath.Glob("../media/0*")
+		for _, file := range files {
+			os.Remove(file)
+		}
+	}
+}
